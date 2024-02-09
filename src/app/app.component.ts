80dfcef2b9bba07ab7c1a6c26f51cbfef90f38d5
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-02-09 11:12:06 -0500

    Funcionamiento de los hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, OnDestroy, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-hooks';
+
+
+
+ export class AppComponent implements OnInit, OnDestroy, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+    title = 'angular-hooks';
+    countador: number =0;
+  counter: number;
+
+    constructor() {
+        console.log('constructor');     
+        this.counter = 1;
+    }
+
+    inc(){
+        console.log('inc');
+        this.counter += 1;
+    }
+
+    ngOnInit(){
+        console.log('ng on init');
+    }
+
+    ngOnDestroy(){
+        console.log('ngOnDestroy');
+    }
+
+    ngDoCheck(){
+        console.log('ngDoCheck');
+    }
+
+    ngOnChanges(){
+        console.log('ngOnChanges');
+    }
+
+    ngAfterContentInit(){
+        console.log('ngAfterContentInit');
+    }
+
+    ngAfterContentChecked(){
+        console.log('ngAfterContentChecked');
+    }
+
+    ngAfterViewInit(){
+        console.log('ngAfterViewInit');
+    }
+    ngAfterViewChecked(){
+        console.log('ngAfterViewChecked');
+    }
 }
+
+
+  
+
